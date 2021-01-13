e648a17c7f8865427333c85b2008e346243f1ce7
Roman Dolgyi <roman@btsolutions.co>
2021-01-13 05:01:57 +0200

    modified:   app.py

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ from starlette.types import ASGIApp
 
 from logger import logger
 
-# TODO: fix
-err
 try:
     settings = __import__(environ.get("AUTHPROXY_SETTINGS", "settings"))
 except ImportError:
